b4f12cacf04eab0f7075eb40498444a891bb17e7
Jessa12 <91742369+Jessa12@users.noreply.github.com>
2022-07-26 15:17:03 +0100

    new changes

--- resources/views/userpage/remind.blade.php
+++ resources/views/userpage/remind.blade.php
@@ -127,7 +127,7 @@
                         <select class="select" id="drugtype" name="drugtype">
                             <option selected disabled>Select Type of Drugs</option>
                             <option value="Pill">Pill</option>
-                            <option value="Medical Leave">Capsule</option>
+                            <option value="Capsule">Capsule</option>
                             <option value="Tablet">Tablet</option>
                             <option value="Syrup">Syrup</option>
                             <option value="Suspension">Suspension</option>
@@ -143,13 +143,13 @@
                     <div class="col-8">
                         <select class="select" id="freqency" name="freqency">
                             <option selected disabled>Select Leave Type</option>
-                            <option value="Pill">Every 2 Hours</option>
-                            <option value="Medical Leave">Every 4 Hours</option>
-                            <option value="Tablet">Once a Day</option>
-                            <option value="Syrup">Twice a Day</option>
-                            <option value="Suspension">Three Times a Day</option>
-                            <option value="Injection">Before Meal</option>
-                            <option value="Sachet">At Bedtime</option>                    
+                            <option value="Every 2 Hours">Every 2 Hours</option>
+                            <option value="Every 4 Hours">Every 4 Hours</option>
+                            <option value="Once a Day">Once a Day</option>
+                            <option value="Twice a Day">Twice a Day</option>
+                            <option value="Three Times a Day">Three Times a Day</option>
+                            <option value="Before Meal">Before Meal</option>
+                            <option value="At Bedtime">At Bedtime</option>                    
                         </select>
                   
                     </div> 	
@@ -313,7 +313,6 @@
             $('#e_number_of_days').val(_this.find('.day').text());
             $('#e_time').val(_this.find('.time').text());
             $('#e_from_date').val(_this.find('.from_date').text());  
-<<<<<<< HEAD
             $('#e_to_date').val(_this.find('.to_date').text());  
             
             var drugtype = (_this.find(".drugtype").text());
@@ -324,9 +323,6 @@
             var _option = '<option selected value="' + freqency+ '">' + _this.find('.freqency').text() + '</option>'
             $( _option).appendTo("#e_freqency");
           
-=======
-            $('#e_to_date').val(_this.find('.to_date').text());           
->>>>>>> 4cd81270433c7269ff076312e56d2462bbb4854a
         });
     </script>
     {{-- delete js for delete button --}}
