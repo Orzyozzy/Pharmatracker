3f5727a62d42d61bbdb8ae40d6ffc87768c0c148
Orzyozzy <sabbytherainbow@gmail.com>
2022-08-09 02:38:43 +0800

    text

--- app/Console/Commands/DailyMessage.php
+++ app/Console/Commands/DailyMessage.php
@@ -12,7 +12,7 @@ class DailyMessage extends Command
      *
      * @var string
      */
-    protected $signature = 'SMS';
+    protected $signature = 'SMS:Twice';
 
     /**
      * The console command description.
