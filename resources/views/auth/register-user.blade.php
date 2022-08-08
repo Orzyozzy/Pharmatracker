8be942394939c302e01763beed91dfb1d2ccf515
Orzozzy <sabbytherainbow@gmail.com>
2022-08-08 15:10:02 -0700

    unknown

--- resources/views/auth/register-user.blade.php
+++ resources/views/auth/register-user.blade.php
@@ -94,7 +94,7 @@
                                     </span>
                                 @enderror
                             </div>
-                        </div>
+                        </div>www
 
                         <div class="row mb-3">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
